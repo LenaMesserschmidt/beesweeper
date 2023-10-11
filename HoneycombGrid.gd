bb2b47f1ae9f10cd19b6cc0d450ee2a8428a2e82
Lena Messerschmidt <messerschmidt.lena@gmail.com>
2023-10-11 23:07:34 +0200

    Recursively handle clicking on empty cells

--- HoneycombGrid.gd
+++ HoneycombGrid.gd
@@ -24,6 +24,7 @@ const TILE_SET_ID = 0
 const DEFAULT_LAYER = 0
 
 var cells_with_grubs = []
+var cells_checked_recursively = []
 
 func _ready():
 	clear_layer(DEFAULT_LAYER)
@@ -51,7 +52,48 @@ func on_cell_clicked(cell_coord: Vector2i):
 		print("YOU LOSE")
 		return
 	
+	cells_checked_recursively.append(cell_coord)
 	
+	handle_cells(cell_coord)
+
+func handle_cells(cell_coord: Vector2i):
+	var tile_data = get_cell_tile_data(DEFAULT_LAYER, cell_coord)
+	
+	# pressed outside grid
+	if tile_data == null:
+		return
+	
+	var cell_has_grub = tile_data.get_custom_data("has_grub")
+	
+	if cell_has_grub:
+		return
+	
+	var grub_count = get_surrounding_cells_grub_count(cell_coord)
+	
+	if grub_count == 0:
+		set_tile_cell(cell_coord, "CLEAR")
+		var surrounding_cells = get_surrounding_cells(cell_coord)
+		for cell in surrounding_cells:
+			handle_surrounding_cell(cell)
+	else:
+		set_tile_cell(cell_coord, "%d" % grub_count)
+
+func handle_surrounding_cell(cell_coord: Vector2i):
+	if cells_checked_recursively.has(cell_coord):
+		return
+	
+	cells_checked_recursively.append(cell_coord)
+	handle_cells(cell_coord)
+
+func get_surrounding_cells_grub_count(cell_coord: Vector2i):
+	var grub_count = 0
+	var surrounding_cells = get_surrounding_cells(cell_coord)
+	for cell in surrounding_cells:
+		var tile_data = get_cell_tile_data(DEFAULT_LAYER, cell)
+		if tile_data and tile_data.get_custom_data("has_grub"):
+			grub_count += 1
+	
+	return grub_count
 
 func place_grubs():
 	for i in number_of_grubs:
